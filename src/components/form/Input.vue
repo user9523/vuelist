e33fdb96f4d82c77607f39983037be22017c01c5
ylh <ylh@example.com>
2020-01-16 00:25:01 +0800

    from组件的init

---
+++ src/components/form/Input.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" :value="value" @input="onInput" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      dafault: ""
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.$emit("input", e.target.value);
+    }
+  }
+};
+</script>
+<style lang="scss" scoped></style>
---
+++ src/components/form/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>Elment表单</h3>
+  <K-input></K-input>
+</template>
+<script>
+import KInput from "./input";
+export default {
+  components: {
+    KInput
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped></style>
