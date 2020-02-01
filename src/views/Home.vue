43ca5ccbe3b8b1246694f4121afc83c74820e76a
ylh <ylh@example.com>
2020-02-01 13:09:45 +0800

    递归组件的实现

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,22 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <FormSample></FormSample>
+    <!-- <FormSample></FormSample> -->
+    <Tree></Tree>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
-import FormSample from "@/components/form/index.vue";
+// import FormSample from "@/components/form/index.vue";
+import Tree from "@/components/tree/index.vue";
 
 export default {
   name: "home",
   components: {
-    FormSample
+    // FormSample,
+    Tree
   }
 };
 </script>
